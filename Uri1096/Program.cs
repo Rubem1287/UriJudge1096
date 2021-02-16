6d4cafc3d5d5aa5280136eb94e46344036faae2f
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:08:36 -0300

    Add files via upload

---
+++ Uri1096/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Uri1096
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("I=1 ");
+            Console.WriteLine(" J=7");
+            Console.WriteLine();
+            Console.Write("I=1 ");
+            Console.WriteLine(" J=6");
+            Console.WriteLine();
+            Console.Write("I=1 ");
+            Console.WriteLine(" J=5");
+            Console.WriteLine();
+            Console.Write("I=3 ");
+            Console.WriteLine(" J=7");
+            Console.WriteLine();
+            Console.Write("I=3 ");
+            Console.WriteLine(" J=6");
+            Console.WriteLine();
+            Console.Write("I=3 ");
+            Console.WriteLine(" J=5");
+            Console.WriteLine();
+            Console.Write("I=5 ");
+            Console.WriteLine(" J=7");
+            Console.WriteLine();
+            Console.Write("I=5 ");
+            Console.WriteLine(" J=6");
+            Console.WriteLine();
+            Console.Write("I=5 ");
+            Console.WriteLine(" J=5");
+            Console.WriteLine();
+            Console.Write("I=7 ");
+            Console.WriteLine(" J=7");
+            Console.WriteLine();
+            Console.Write("I=7 ");
+            Console.WriteLine(" J=6");
+            Console.WriteLine();
+            Console.Write("I=7 ");
+            Console.WriteLine(" J=5");
+            Console.WriteLine();
+            Console.Write("I=9 ");
+            Console.WriteLine(" J=7");
+            Console.WriteLine();
+            Console.Write("I=9 ");
+            Console.WriteLine(" J=6");
+            Console.WriteLine();
+            Console.Write("I=9 ");
+            Console.WriteLine(" J=5");
+        }
+    }
+}
